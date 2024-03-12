d8c33763fbf682f443feeeb652150117149e1065
Luna <basikallyluna@gmail.com>
2024-03-12 14:53:54 +0200

    Add windows venv, remove curses code

--- interface.py
+++ interface.py
@@ -1,18 +1,8 @@
-import curses
+import click
 import json
 
 def first_launch_config():
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    curses.curs_set(False)
-    if curses.has_colors():
-        curses.start_color()
-
-    stdscr.addstr("LOL")
-    stdscr.refresh()
-    stdscr.getch()
-    curses.endwin()
+    
 
 with open('config.json', 'r') as f:
     config = json.load(f)
