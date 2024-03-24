45cb1787e4f54ae214fd7948a4ae7eb0257921c8
LunaXCBN <basikallyluna@gmail.com>
2024-03-24 10:16:09 +0200

    Redid weather variables

---
+++ maps.py
@@ -0,0 +1,13 @@
+weather_vars = {
+        "Temperature": "temperature",
+        "Relative Humidity": "relative_humidity",
+        "Apparent Temperature": "apparent_temperature",
+        "Is Day or Night": "is_day_or_night",
+        "Rain": "rain",
+        "Showers": "showers",
+        "Snowfall": "snowfall",
+        "Cloud cover Total": "cloud_cover_total",
+        "Wind Speed": "wind_speed",
+        "Wind Direction": "wind_direction",
+        "Wind Gusts": "wind_gusts"
+        }
